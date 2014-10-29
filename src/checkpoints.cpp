9f7922b9ae5bb44e8c7ed8a27ef8c607402d7b11
CryptoParts <cryptoparts@gmail.com>
2014-10-29 06:27:46 +0000

    Update checkpoints.cpp

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -5,11 +5,13 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include <boost/assign/list_of.hpp> // for 'map_list_of()'
+
 #include <boost/foreach.hpp>
 
 #include "checkpoints.h"
 
 #include "main.h"
+
 #include "uint256.h"
 
 namespace Checkpoints
