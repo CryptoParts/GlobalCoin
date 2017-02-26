12fc8d15d437dc5af7eb3118bc2964c011a145c9
Greg Griffith <tfa.productions.1337@gmail.com>
2017-02-26 03:58:15 -0500

    undo noblks change
    undo the noblks change as a min protocol change has been implemented
    instead

--- src/version.h
+++ src/version.h
@@ -43,7 +43,7 @@ static const int CADDR_TIME_VERSION = 31402;
 
 // only request blocks from nodes outside this range of versions
 static const int NOBLKS_VERSION_START = 1;
-static const int NOBLKS_VERSION_END = 70004;
+static const int NOBLKS_VERSION_END = 70000;
 
 // BIP 0031, pong message, is enabled for all versions AFTER this one
 static const int BIP0031_VERSION = 60000;
